fcea0861774a5f53c9007ed9962d998a45bdf1fe
Soichi Tamashiro <wyvernys@live.com>
2020-08-09 18:41:24 -0500

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   default.kv
    	modified:   default.py
    	new file:   pesaje/ciclos_pesaje.kv
    	new file:   pesaje/ciclos_pesaje.py
    	new file:   pesaje/pesaje_general.kv
    	new file:   pesaje/pesaje_general.py
    	new file:   signin/signin.kv
    	new file:   signin/signin.py

--- screens/default/default.py
+++ screens/default/default.py
@@ -9,6 +9,7 @@ from kivy.uix.button import Button
 from kivy.uix.tabbedpanel import TabbedPanel
 
 from utils.table import TableWid
+from pesaje.pesaje_general import PesajeGeneral
 
 from kivy.lang import Builder
 
@@ -20,11 +21,13 @@ Config.set("graphics", "minimum_height", "600")
 
 
 class MainWid(BoxLayout):
-    table_widget = TableWid()
+    # table_widget = TableWid()
+    pesaje_gen_widget = PesajeGeneral()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.ids.container2.add_widget(self.table_widget)
+        # self.ids.container2.add_widget(self.table_widget)
+        self.ids.scrn_pesaje_gen.add_widget(self.pesaje_gen_widget)
     # pesaje_gen = self.ids.scrn_pesaje_gen
     #
 
