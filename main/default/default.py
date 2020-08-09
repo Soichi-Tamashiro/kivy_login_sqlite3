e1d4f94ca467dc23966397fd826aabe9f2533fe0
Soichi Tamashiro <wyvernys@live.com>
2020-08-09 05:00:34 -0500

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   ../../main/default/default.kv
    	modified:   ../../main/default/default.py

--- main/default/default.py
+++ main/default/default.py
@@ -6,6 +6,7 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.button import Button
+from kivy.uix.tabbedpanel import TabbedPanel
 
 from kivy.lang import Builder
 
@@ -19,6 +20,26 @@ Config.set("graphics", "minimum_height", "600")
 class MainWid(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+    # pesaje_gen = self.ids.scrn_pesaje_gen
+    #
+
+    def change_screen(self, instance):
+        if instance.text == '- Bascula':
+            self.ids.scrn_mngr.current = 'scrn_bascula'
+        elif instance.text == '- Pesaje General':
+            self.ids.scrn_mngr.current = 'scrn_pesaje_gen'
+        elif instance.text == '- Empresa':
+            self.ids.scrn_mngr.current = 'scrn_empresa'
+        else:
+            pass
+
+    def change_screen_pesaje(self, instance):
+        if instance.text == 'Gestion Pesaje Vehiculo':
+            self.ids.scrn_mngr.current = 'scrn_pesaje'
+        elif instance.text == 'Gestion Consulta Vehiculo':
+            self.ids.scrn_mngr.current = 'scrn_consulta'
+        else:
+            pass
 
 
 class Default(App):
